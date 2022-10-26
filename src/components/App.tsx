0280d2459b02e8e3ce864a797abbae065e52fc16
Дмитрий Фарина <esohyki@yandex.by>
2022-10-26 10:07:17 +0300

    fix index file

--- src/components/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { withBitrixLayout } from './withBitrixLayout'
-
-const App = () => {
-  return <h2>НАШЕ ПРИЛОЖЕНИЕ</h2>
-}
-
-export default withBitrixLayout(App)
--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -2,11 +2,5 @@ import * as React from 'react'
 import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
-import App from '../src/components/App'
 
-describe('Common render', () => {
-  it('Renders without crashing', () => {
-    render(<App />)
-  })
-})
 
